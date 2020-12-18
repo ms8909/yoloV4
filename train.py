081adbd66b5289af9fd70df2afe89c6f32c10303
Muddassar Sharif <ms8909@nyu.edu>
2020-12-18 05:46:54 +0500

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,4 @@
-#================================================================
-#
-#   File name   : train.py
-#   Author      : PyLessons
-#   Created date: 2020-08-06
-#   Website     : https://pylessons.com/
-#   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
-#   Description : used to train custom object detector
-#
-#================================================================
+
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
